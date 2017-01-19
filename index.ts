504e95bd1e9c1065e4d328bafe769b39e0126230
augustine <ashramwen@gmail.com>
2017-01-19 11:38:22 +0900

    ts

--- index.ts
+++ index.ts
@@ -1,23 +1,31 @@
-"use strict";
-var request = require("request");
-var mqtt = require("mqtt");
-var Q = require("q");
-var low = require("lowdb");
-var db = new low('db.json');
-var GatewayAgent = (function () {
-    function GatewayAgent() {
+import request = require('request');
+import mqtt = require('mqtt');
+import Q = require('q');
+import low = require('lowdb');
+const db = new low('db.json');
+
+export default class GatewayAgent {
+    constructor() {
+
     }
-    GatewayAgent.prototype.init = function (appID, appKey, site) {
-        this.appID = appID;
-        this.appKey = appKey;
-        this.site = site;
-        db.set('app.appID', appID).value();
-        db.set('app.appKey', appKey).value();
-        db.set('app.site', site).value();
-    };
-    GatewayAgent.prototype.onboardGatewayByOwner = function (ownerToken, ownerID, vendorThingID, password, type, properties) {
-        var deferred = Q.defer();
-        var options = {
+
+    appID: string;
+    appKey: string;
+    site: string;
+    messageHandler;
+
+    init(appID, appKey, site) {
+        this.appID = appID
+        this.appKey = appKey
+        this.site = site
+        db.set('app.appID', appID).value()
+        db.set('app.appKey', appKey).value()
+        db.set('app.site', site).value()
+    }
+
+    onboardGatewayByOwner(ownerToken, ownerID, vendorThingID, password, type, properties) {
+        let deferred = Q.defer()
+        let options = {
             method: 'POST',
             url: this.site + '/thing-if/apps/' + this.appID + '/onboardings',
             headers: {
@@ -32,22 +40,26 @@ var GatewayAgent = (function () {
                 'layoutPosition': 'GATEWAY'
             })
         };
+
         request(options, function (error, response, body) {
-            if (error)
-                deferred.reject(new Error(error));
-            db.set('gateway.vendorThingID', vendorThingID).value();
-            db.set('gateway.thingID', JSON.parse(body).thingID).value();
-            db.set('gateway.accessToken', JSON.parse(body).accessToken).value();
+            if (error) deferred.reject(new Error(error))
+
+            db.set('gateway.vendorThingID', vendorThingID).value()
+            db.set('gateway.thingID', JSON.parse(body).thingID).value()
+            db.set('gateway.accessToken', JSON.parse(body).accessToken).value()
+
             deferred.resolve({
                 gatewayInfo: JSON.parse(body)
-            });
+            })
         });
-        return deferred.promise;
-    };
-    GatewayAgent.prototype.onboardEndnodeByOwner = function (ownerToken, ownerID, endNodeVendorThingID, endNodePassword, type, properties) {
-        var gatewayThingID = db.get('gateway.thingID');
-        var deferred = Q.defer();
-        var options = {
+
+        return deferred.promise
+    }
+
+    onboardEndnodeByOwner(ownerToken, ownerID, endNodeVendorThingID, endNodePassword, type, properties) {
+        let gatewayThingID = db.get('gateway.thingID')
+        let deferred = Q.defer()
+        let options = {
             method: 'POST',
             url: this.site + '/thing-if/apps/' + this.appID + '/onboardings',
             headers: {
@@ -62,74 +74,78 @@ var GatewayAgent = (function () {
                 'endNodeThingType': type,
                 'owner': 'USER:' + ownerID
             })
-        };
+        }
+
         request(options, function (error, response, body) {
-            if (error)
-                deferred.reject(new Error(error));
+            if (error) deferred.reject(new Error(error))
+
             if (db.get('endNodes').find({ 'vendorThingID': endNodeVendorThingID }).value())
                 db.get('endNodes').find({ 'vendorThingID': endNodeVendorThingID }).assign({
                     vendorThingID: endNodeVendorThingID,
                     accessToken: JSON.parse(body).accessToken,
                     endNodeThingID: JSON.parse(body).endNodeThingID
-                }).value();
+                }).value()
             else
                 db.get('endNodes').push({
                     vendorThingID: endNodeVendorThingID,
                     accessToken: JSON.parse(body).accessToken,
                     endNodeThingID: JSON.parse(body).endNodeThingID
-                }).value();
-            deferred.resolve(JSON.parse(body));
-        });
-        return deferred.promise;
-    };
-    GatewayAgent.prototype.updateEndnodeState = function (thingID, state) {
-    };
-    GatewayAgent.prototype.updateEndnodeConnectivity = function (thingID, connected) {
-    };
-    GatewayAgent.prototype.startCommandReceiver = function (chainInput) {
-        var deferred = Q.defer();
-        var gatewayInfo = chainInput.gatewayInfo;
-        var mqttEndpoint = gatewayInfo.mqttEndpoint;
-        var option = {
+                }).value()
+            deferred.resolve(JSON.parse(body))
+        })
+
+        return deferred.promise
+    }
+
+    updateEndnodeState(thingID, state) {
+
+    }
+
+    updateEndnodeConnectivity(thingID, connected) {
+
+    }
+
+    startCommandReceiver(chainInput) {
+        let deferred = Q.defer()
+        let gatewayInfo = chainInput.gatewayInfo
+        let mqttEndpoint = gatewayInfo.mqttEndpoint;
+        let option = {
             'port': mqttEndpoint.portTCP,
             'clientId': mqttEndpoint.mqttTopic,
             'username': mqttEndpoint.username,
             'password': mqttEndpoint.password,
             'reconnectPeriod': 55000,
             'keepalive': 60
-        };
-        console.log(mqttEndpoint);
-        var client = mqtt.connect('tcp://' + mqttEndpoint.host, option);
+        }
+        console.log(mqttEndpoint)
+        let client = mqtt.connect('tcp://' + mqttEndpoint.host, option);
         client.on('connect', function (connack) {
             if (!connack.sessionPresent) {
                 client.subscribe(mqttEndpoint.mqttTopic, {
                     qos: 0,
                     retain: false
                 }, function (err, granted) {
-                    if (err)
-                        deferred.reject(err);
+                    if (err) deferred.reject(err)
                 });
+            } else {
+                throw new Error('error connecting to MQTT broker')
             }
-            else {
-                throw new Error('error connecting to MQTT broker');
-            }
-        });
+        })
         client.on('error', function (error) {
-            throw new Error(error);
-        });
+            throw new Error(error)
+        })
         client.on('message', function (topic, message, packet) {
-            var i, messageStr = '';
+            let i, messageStr = '';
             for (i = 0; i < message.length; i++) {
                 messageStr += '%' + ('0' + message[i].toString(16)).slice(-2);
             }
             messageStr = decodeURIComponent(messageStr);
-            this.messageHandler(messageStr);
-        });
-    };
-    GatewayAgent.prototype.setOnCommandMessage = function (messageHandler) {
-        this.messageHandler = messageHandler;
-    };
-    return GatewayAgent;
-}());
-exports.__esModule = true;
-exports["default"] = GatewayAgent;
+            this.messageHandler(messageStr)
+        })
+    }
+
+    setOnCommandMessage(messageHandler) {
+        this.messageHandler = messageHandler
+    }
+}
+
