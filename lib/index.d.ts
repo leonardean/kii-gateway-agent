e7acf96d4efe1c6f683ebb662a1efbef9752d0ec
augustine <ashramwen@gmail.com>
2017-01-20 14:47:45 +0900

    resource folder

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,11 +1,13 @@
-export declare let appID: string;
-export declare let appKey: string;
-export declare let site: string;
-export declare function init(_appID: any, _appKey: any, _site: any): void;
-export declare function onboardGatewayByOwner(ownerToken: any, ownerID: any, vendorThingID: any, password: any, type: any, properties: any): any;
-export declare function onboardEndnodeByOwner(ownerToken: any, ownerID: any, endNodeVendorThingID: any, endNodePassword: any, type: any, properties: any): any;
-export declare function updateEndnodeState(ownerToken: any, endNodeThingID: any, states: any): any;
-export declare function updateEndnodeConnectivity(ownerToken: string, endNodeThingID: string, online: boolean): any;
-export declare function detectEndnodeOnboardingStatus(endNodeVendorThingID: string): boolean;
-export declare function startCommandReceiver(chainInput: any): void;
-export declare function setOnCommandMessage(messageHandler: any): void;
+export declare module gatewayAgent {
+    let appID: string;
+    let appKey: string;
+    let site: string;
+    function init(_appID: any, _appKey: any, _site: any): void;
+    function onboardGatewayByOwner(ownerToken: any, ownerID: any, vendorThingID: any, password: any, type: any, properties: any): any;
+    function onboardEndnodeByOwner(ownerToken: any, ownerID: any, endNodeVendorThingID: any, endNodePassword: any, type: any, properties: any): any;
+    function updateEndnodeState(ownerToken: any, endNodeThingID: any, states: any): any;
+    function updateEndnodeConnectivity(ownerToken: string, endNodeThingID: string, online: boolean): any;
+    function detectEndnodeOnboardingStatus(endNodeVendorThingID: string): boolean;
+    function startCommandReceiver(chainInput: any): void;
+    function setOnCommandMessage(messageHandler: any): void;
+}
